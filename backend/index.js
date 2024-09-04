1e2ed615699061559f11b8092c43c2afb7e103e0
Cdogonza <gonzalo.paz@correo.ucu.edu.uy>
2024-09-03 23:56:36 -0300

    dadd

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,13 @@ const app = express();
 let cors = require('cors');
 const bodyparser = require('body-parser');
 
-
-app.use(cors());
+const cors=require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+app.use(cors(corsOptions));
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended: false}));
 
@@ -15,4 +20,5 @@ app.use(require('./routes/correo.Routes'));
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
+
 });
