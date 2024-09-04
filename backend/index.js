45ccf8888da94a54fe8a67deaa748fab2e65307a
Cdogonza <gonzalo.paz@correo.ucu.edu.uy>
2024-09-04 00:20:18 -0300

    dadde

--- backend/index.js
+++ backend/index.js
@@ -4,12 +4,14 @@ let cors = require('cors');
 const bodyparser = require('body-parser');
 
 const cors=require("cors");
-const corsOptions ={
-   origin:'*', 
-   credentials:true,            //access-control-allow-credentials:true
-   optionSuccessStatus:200,
-}
-app.use(cors(corsOptions));
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET , PUT , POST , DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type, x-requested-with");
+    next(); // Important
+})
+
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended: false}));
 
@@ -20,5 +22,5 @@ app.use(require('./routes/correo.Routes'));
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
-
+    // Access-Control-Allow-Origin: *
 });
