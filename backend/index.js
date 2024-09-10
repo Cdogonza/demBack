7d578318951dc2ce90fb8904daacc6623877aa34
Cdogonza <gonzalo.paz@correo.ucu.edu.uy>
2024-09-10 02:01:13 -0300

    sdad

--- backend/index.js
+++ backend/index.js
@@ -1,32 +1,23 @@
 const express  = require('express');
 const app = express();
-const bodyparser = require('body-parser');
+// const bodyparser = require('body-parser');
 
-const cors=require("cors");
-app.use(cors({origin:"*"
-,credentials:true,
-methods:"GET,PUT,POST,DELETE",
-allowedHeaders:"Content-Type,Authorization",
-preflightContinue:false,
-optionsSuccessStatus:204,
-maxAge:86400,
-exposedHeaders:["Content-Type","Authorization"],
+// const cors=require("cors");
+// app.use(cors({origin:"*"
+// ,credentials:true,
+// methods:"GET,PUT,POST,DELETE",
+// allowedHeaders:"Content-Type,Authorization",
+// preflightContinue:false,
+// optionsSuccessStatus:204,
+// maxAge:86400,
+// exposedHeaders:["Content-Type","Authorization"],
 
-}));
-
-
-// app.use((req, res, next) =>{
-//    res.header("Access-Control-Allow-Headers","*");
-//     res.header('Access-Control-Allow-Credentials', true);
-//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-//     res.header('Access-Control-Allow-Origin', '*');
-           
-//     next()
-// })
+// }));
 
 
-app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({extended: false}));
+
+// app.use(bodyparser.json());
+// app.use(bodyparser.urlencoded({extended: false}));
 
 const port = process.env.PORT || 3000;
 
